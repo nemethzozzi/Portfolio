c4be286b5cd8936bd21278c6fd7a27d190ab1fd0
Nemeth Zoltan <nemethzoltan0524@gmail.com>
2023-12-04 23:52:15 +0100

    updating

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -18,98 +18,84 @@ const Skills = ({ darkMode }) => {
 
   return (
     <div id="skills" ref={ref} onClick={handleContainerClick} className="cursor-pointer">
-      <div className={`p-4 h-screen ml-20 mr-20 ${darkMode ? 'bg-dark' : 'bg-light'}`}>
-        <h2 className={`text-8xl text-center font-bold ${darkMode ? 'text-white' : 'text-black'} mb-40 mt-10`}>
+      <div className={`p-4 ${darkMode ? 'bg-dark' : 'bg-light'}`}>
+        <h2 className={`text-5xl sm:text-6xl md:text-7xl lg:text-8xl text-center font-bold ${darkMode ? 'text-white' : 'text-black'} mb-10 mt-5`}>
           My Skills
         </h2>
         <CardFlip isFlipped={isFlipped} flipDirection="horizontal">
           {/* Frontend Content */}
-          <div key="frontend"
-          className="space-y-8 bg-white bg-opacity-10 rounded-2xl flex items-center border-gray-500 border-4"
-          style={{
-            backgroundImage: 'url("frontend_background.png")',
-            backgroundSize: 'contain',  
-            backgroundPosition: 'right center',
-            backgroundRepeat: 'no-repeat', 
-          }}>
-            <div className="flex-1 m-10">
-              <h3 className={`text-4xl font-bold mb-10 ml-5 ${darkMode ? 'text-white' : 'text-black'}`}>
+          <div
+            key="frontend"
+            className="space-y-4 md:space-y-8 bg-white bg-opacity-10 rounded-2xl flex flex-col md:flex-row items-center border-gray-500 border-4"
+            style={{
+              backgroundImage: 'url("frontend_background.png")',
+              backgroundSize: 'contain',
+              backgroundPosition: 'right center',
+              backgroundRepeat: 'no-repeat',
+            }}
+          >
+            <div className="flex-1 md:m-10">
+              <h3 className={`text-2xl md:text-4xl font-bold mb-5 md:mb-10 ml-2 md:ml-5 ${darkMode ? 'text-white' : 'text-black'}`}>
                 Frontend
               </h3>
               {frontendData.map((skill, index) => (
-                <div key={index} className={`bg-black bg-opacity-30 backdrop-filter backdrop-blur-md ${darkMode ? 'text-white' : 'text-black'} flex items-center flex-1 ml-5`}>
-                      <img
-                        src={skill.icon[0]}
-                        alt={`${skill.skill} icon`}
-                        className="w-8 h-8"
-                      />
-                      <div
-                        className={`h-6 ml-4 rounded-lg mb-5 mt-5 ${skill.color} bar ${
-                          inView ? 'animate-grow-bar' : ''
-                        }`}
-                        style={{
-                          width: inView ? `${skill.level}%` : '0',
-                          transition: inView ? 'width 1.5s' : '0s',
-                        }}
-                      >
-                        {inView ? `${skill.level}%` : '0%'}
-                      </div>
-                  <div className="flex-1">
-                    <span className={`ml-4 ${darkMode ? 'text-white' : 'text-black'}`}>
-                      {skill.label}
-                    </span>
+                <div key={index} className={`bg-black bg-opacity-30 backdrop-filter backdrop-blur-md ${darkMode ? 'text-white' : 'text-black'} flex items-center flex-1 ml-2 md:ml-5 md:`}>
+                  <img src={skill.icon[0]} alt={`${skill.skill} icon`} className="w-6 h-6 md:w-8 md:h-8" />
+                  <div
+                    className={`h-4 md:h-6 ml-2 md:ml-4 rounded-lg mb-2 md:mb-5 mt-2 md:mt-5 ${skill.color} bar ${
+                      inView ? 'animate-grow-bar' : ''
+                    }`}
+                    style={{
+                      width: inView ? `${skill.level}%` : '0',
+                      transition: inView ? 'width 1.5s' : '0s',
+                    }}
+                  >
+                    {inView ? `${skill.level}%` : '0%'}
                   </div>
+                  <span className={`ml-2 ${darkMode ? 'text-white' : 'text-black'}`}>{skill.label}</span>
                 </div>
               ))}
             </div>
-            <div className="w-2/5 h-full flex items-center justify-center">
+            <div className="w-full md:w-2/5 h-48 md:h-full flex items-center justify-center">
               <img src="frontend.png" alt="Frontend" className="w-2/3 h-auto" />
             </div>
           </div>
-            
+
           {/* Backend Content */}
-          <div key="backend"
-          className="space-y-8 bg-white bg-opacity-10 rounded-2xl flex items-center border-gray-500 border-4"
-          style={{
-            backgroundImage: 'url("backend_background.png")',
-            backgroundSize: 'contain', 
-            backgroundPosition: 'left center',
-            backgroundRepeat: 'no-repeat', 
-          }}>
-            <div className="w-2/5 h-full flex items-center justify-center">
-              <img src="backend.png" alt="Backend" className="w-2/3 h-auto" />
-            </div>
-            <div className="flex-1 m-10">
-              <h3 className={`text-right text-4xl font-bold mb-10 mr-5 ${darkMode ? 'text-white' : 'text-black'}`}>
+          <div
+            key="backend"
+            className="space-y-4 md:space-y-8 bg-white bg-opacity-10 rounded-2xl flex flex-col md:flex-row items-center border-gray-500 border-4"
+            style={{
+              backgroundImage: 'url("backend_background.png")',
+              backgroundSize: 'contain',
+              backgroundPosition: 'right center',
+              backgroundRepeat: 'no-repeat',
+            }}
+          >
+            <div className="flex-1 md:m-10">
+              <h3 className={`text-2xl md:text-4xl font-bold mb-5 md:mb-10 ml-2 md:ml-5 ${darkMode ? 'text-white' : 'text-black'}`}>
                 Backend
               </h3>
               {backendData.map((skill, index) => (
-                <div key={index} className={`bg-black bg-opacity-30 backdrop-filter backdrop-blur-md ${darkMode ? 'text-white' : 'text-black'} flex items-center`}>
-                    <div className="flex items-center justify-end w-full">
-                      <div
-                        className={`h-6 mr-4 rounded-lg mb-5 mt-5 ${skill.color} bar ${
-                          inView ? 'animate-grow-bar' : ''
-                        }`}
-                        style={{
-                          width: inView ? `${skill.level}%` : '0',
-                          transition: inView ? 'width 1.5s' : '0s',
-                        }}
-                      >
-                        {inView ? `${skill.level}%` : '0%'}
-                      </div>
-                      <img
-                        src={skill.icon[0]}
-                        alt={`${skill.skill} icon`}
-                        className="w-8 h-8"
-                      />
-                    </div>
-                  <div className="flex-1">
-                    <span className={`mr-4 ${darkMode ? 'text-white' : 'text-black'}`}>
-                      {skill.label}
-                    </span>
+                <div key={index} className={`bg-black bg-opacity-30 backdrop-filter backdrop-blur-md ${darkMode ? 'text-white' : 'text-black'} flex items-center flex-1 ml-2 md:ml-5 md:`}>
+                  <img src={skill.icon[0]} alt={`${skill.skill} icon`} className="w-6 h-6 md:w-8 md:h-8" />
+                  <div
+                    className={`h-4 md:h-6 ml-2 md:ml-4 rounded-lg mb-2 md:mb-5 mt-2 md:mt-5 ${skill.color} bar ${
+                      inView ? 'animate-grow-bar' : ''
+                    }`}
+                    style={{
+                      width: inView ? `${skill.level}%` : '0',
+                      transition: inView ? 'width 1.5s' : '0s',
+                    }}
+                  >
+                    {inView ? `${skill.level}%` : '0%'}
                   </div>
+                  <span className={`ml-2 ${darkMode ? 'text-white' : 'text-black'}`}>{skill.label}</span>
                 </div>
               ))}
+            </div>
+            <div className="w-full md:w-2/5 h-48 md:h-full flex items-center justify-center">
+              <img src="backend.png" alt="Backend" className="w-2/3 h-auto" />
             </div>
           </div>
         </CardFlip>
