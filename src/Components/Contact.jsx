6ae2c0b3cfa6a5e8ddd8d07fcda104b1d745c8a4
Nemeth Zoltan <nemethzoltan0524@gmail.com>
2023-12-10 12:50:50 +0100

    updating to be responsive

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,6 +2,34 @@ import React, { useRef } from 'react';
 import { useMediaQuery } from 'react-responsive';
 
 const Contact = ({ darkMode }) => {
+  const contactItems = [
+    {
+      label: "GitHub",
+      text: "/nemethzozzi",
+      icon: "icons/github.png",
+    },
+    {
+      label: "Email",
+      text: "nemethzoltan0524@gmail.com",
+      icon: "icons/email.png",
+    },
+    {
+      label: "LinkedIn",
+      text: "/nemethzoltan0524",
+      icon: "icons/linkedin.png",
+    },
+    {
+      label: "Discord",
+      text: "/nemethzozzi",
+      icon: "icons/discord.png",
+    },
+    {
+      label: "Phone",
+      text: "06 70 601 0524",
+      icon: "icons/phone.png",
+    },
+  ];
+
   const ref = useRef(null);
   const isMobile = useMediaQuery({ maxWidth: 767 });
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1023 });
@@ -11,7 +39,26 @@ const Contact = ({ darkMode }) => {
       <div className={`flex items-center justify-center ${isMobile ? 'h-screen' : 'min-h-screen'} ${darkMode ? 'bg-dark' : 'bg-light'}`}>
         <div className={`max-w-screen-xl mx-8 p-10 ${isMobile ? 'w-full' : isTablet ? 'w-3/4' : 'w-2/3'}`}>
           <h1 className={`text-4xl md:text-6xl text-center font-bold animate-pulse ${darkMode ? 'text-white' : 'text-black'} mb-8`}>Contact Me</h1>
-          <img src="qr.png" alt="QR Code" className={`w-${isMobile ? 'full' : '1/2'} mx-auto max-w-full`} />
+
+          {isMobile || isTablet ? (
+            // Render contact items in mobile and tablet mode
+            <div className={`grid grid-cols-1 ${isTablet ? 'sm:grid-cols-2' : ''} gap-4 sm:gap-8 mt-8 border-4 border-gray-500 rounded-3xl p-8 shadow-md ${darkMode ? 'bg-dark-secondary' : 'bg-light-secondary'}`}>
+              {contactItems.map((item, index) => (
+                <div key={index} className={`flex items-center text-center ${darkMode ? 'text-white' : 'text-black'}`}>
+                  <div className="flex flex-col items-center mt-2">
+                    <img src={item.icon} alt={`${item.icon} icon`} className="w-8 h-8" />
+                  </div>
+                  <div className="flex flex-col items-start mt-2 ml-3">
+                    <span className="text-base font-semibold">{item.label}</span>
+                    <span className="text-xs">{item.text}</span>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            // Render QR code in larger screens
+            <img src="qr.png" alt="QR Code" className="mx-auto w-1/3" />
+          )}
         </div>
       </div>
     </div>
--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -17,40 +17,38 @@ const Skills = ({ darkMode }) => {
   };
 
   return (
-    <div id="skills">
-      <div className={`p-4 ${darkMode ? 'bg-dark' : 'bg-light'}`}>
-        <h2 className={`text-5xl sm:text-6xl md:text-7xl lg:text-8xl text-center font-bold ${darkMode ? 'text-white' : 'text-black'} mb-10 mt-5`}>
-          My Skills
-        </h2>
-        <CardFlip isFlipped={isFlipped} flipDirection="horizontal">
-          {/* Frontend Content */}
-          <div
-            key="frontend"
-            className="cursor-pointer space-y-4 md:space-y-8 bg-white bg-opacity-10 rounded-2xl flex flex-col md:flex-row items-center border-gray-500 border-4"
-            style={{
-              backgroundImage: 'url("frontend_background.png")',
-              backgroundSize: 'contain',
-              backgroundPosition: 'right center',
-              backgroundRepeat: 'no-repeat',
-            }}
-            ref={ref} onClick={handleContainerClick}
-          >
-            <div className="flex-1 md:m-10">
-              <h3 className={`text-2xl md:text-4xl font-bold mb-5 md:mb-10 ml-2 md:ml-5 ${darkMode ? 'text-white' : 'text-black'}`}>
-                Frontend
-              </h3>
-              {frontendData.map((skill, index) => (
-              <div key={index} className={`bg-black bg-opacity-30 backdrop-filter backdrop-blur-md ${darkMode ? 'text-white' : 'text-black'} flex items-center flex-1 ml-2 md:ml-5 md:relative`}>
-                <img src={skill.icon[0]} alt={`${skill.skill} icon`} className="w-6 h-6 md:w-8 md:h-8" />
-
+    <div id="skills" className={`p-2 max-w-screen-md mx-auto ${darkMode ? 'bg-dark' : 'bg-light'}`}>
+      <h2 className={`text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-center font-bold ${darkMode ? 'text-white' : 'text-black'} mb-5 mt-3`}>
+        My Skills
+      </h2>
+      <CardFlip isFlipped={isFlipped} flipDirection="horizontal">
+        {/* Frontend Content */}
+        <div
+          key="frontend"
+          className="cursor-pointer space-y-2 md:space-y-4 bg-white bg-opacity-10 rounded-2xl flex flex-col md:flex-row items-center border-gray-500 border-2"
+          style={{
+            backgroundImage: 'url("frontend_background.png")',
+            backgroundSize: 'contain',
+            backgroundPosition: 'right center',
+            backgroundRepeat: 'no-repeat',
+          }}
+          ref={ref}
+          onClick={handleContainerClick}
+        >
+          <div className="flex-1 md:m-5">
+            <h3 className={`text-xl md:text-2xl font-bold mb-3 md:mb-5 ml-1 md:ml-3 ${darkMode ? 'text-white' : 'text-black'}`}>
+              Frontend
+            </h3>
+            {frontendData.map((skill, index) => (
+              <div key={index} className={`bg-black bg-opacity-20 backdrop-filter backdrop-blur-sm ${darkMode ? 'text-white' : 'text-black'} flex items-center flex-1 ml-1 md:ml-3 md:relative`}>
+                <img src={skill.icon[0]} alt={`${skill.skill} icon`} className="w-4 h-4 md:w-6 md:h-6" />
                 {/* Opacity Bar */}
                 <div
-                  className={`h-6 w-11/12 bg-gray-300 absolute rounded-lg top-1/2 transform -translate-y-1/2 left-2 md:left-12 right-20 md:right-4 ${darkMode ? 'opacity-40' : 'opacity-20'}`}
+                  className={`h-4 w-10/12 bg-gray-300 absolute rounded-lg top-1/2 transform -translate-y-1/2 left-1 md:left-8 right-10 md:right-2 ${darkMode ? 'opacity-30' : 'opacity-10'}`}
                 ></div>
-
                 {/* Colored Bar */}
                 <div
-                  className={`h-4 md:h-6 ml-2 md:ml-4 rounded-lg mb-2 md:mb-5 mt-2 md:mt-5 ${skill.color} bar ${
+                  className={`h-2 md:h-4 ml-1 md:ml-2 rounded-lg mb-1 md:mb-3 mt-1 md:mt-3 ${skill.color} bar ${
                     inView ? 'animate-grow-bar' : ''
                   } relative`}
                   style={{
@@ -61,65 +59,61 @@ const Skills = ({ darkMode }) => {
                 >
                   {inView ? `${skill.level}%` : '0%'}
                 </div>
-
-                <span className={`ml-2 ${darkMode ? 'text-white' : 'text-black'}`}>{skill.label}</span>
+                <span className={`ml-1 ${darkMode ? 'text-white' : 'text-black'}`}>{skill.label}</span>
               </div>
             ))}
-
-            </div>
-            <div className="w-full md:w-2/5 h-48 md:h-full flex items-center justify-center">
-              <img src="frontend.png" alt="Frontend" className="w-2/3 h-auto" />
-            </div>
           </div>
+          <div className="w-full md:w-2/5 h-24 md:h-40 flex items-center justify-center">
+            <img src="frontend.png" alt="Frontend" className="w-1/2 h-auto" />
+          </div>
+        </div>
 
-          {/* Backend Content */}
-          <div
-            key="backend"
-            className="cursor-pointer space-y-4 md:space-y-8 bg-white bg-opacity-10 rounded-2xl flex flex-col md:flex-row items-center border-gray-500 border-4"
-            style={{
-              backgroundImage: 'url("backend_background.png")',
-              backgroundSize: 'contain',
-              backgroundPosition: 'right center',
-              backgroundRepeat: 'no-repeat',
-            }}
-            ref={ref} onClick={handleContainerClick}
-          >
-            <div className="flex-1 md:m-10">
-              <h3 className={`text-2xl md:text-4xl font-bold mb-5 md:mb-10 ml-2 md:ml-5 ${darkMode ? 'text-white' : 'text-black'}`}>
-                Backend
-              </h3>
-              {backendData.map((skill, index) => (
-                <div key={index} className={`bg-black bg-opacity-30 backdrop-filter backdrop-blur-md ${darkMode ? 'text-white' : 'text-black'} flex items-center flex-1 ml-2 md:ml-5 md:`}>
-                  <img src={skill.icon[0]} alt={`${skill.skill} icon`} className="w-6 h-6 md:w-8 md:h-8" />
-                  
-                  {/* Opacity Bar */}
-                  <div
-                    className={`h-6 w-11/12 bg-gray-300 absolute rounded-lg top-1/2 transform -translate-y-1/2 left-2 md:left-12 right-20 md:right-4 ${darkMode ? 'opacity-40' : 'opacity-20'}`}
-                  ></div>
-
-                  {/* Colored Bar */}
-                  <div
-                    className={`h-4 md:h-6 ml-2 md:ml-4 rounded-lg mb-2 md:mb-5 mt-2 md:mt-5 ${skill.color} bar ${
-                      inView ? 'animate-grow-bar' : ''
-                    } relative`}
-                    style={{
-                      width: inView ? `${skill.level}%` : '0',
-                      transition: inView ? 'width 1.5s' : '0s',
-                      opacity: '1',
-                    }}
-                  >
-                    {inView ? `${skill.level}%` : '0%'}
-                  </div>
-                  <span className={`ml-2 ${darkMode ? 'text-white' : 'text-black'}`}>{skill.label}</span>
+        {/* Backend Content */}
+        <div
+          key="backend"
+          className="cursor-pointer space-y-2 md:space-y-4 bg-white bg-opacity-10 rounded-2xl flex flex-col md:flex-row items-center border-gray-500 border-2"
+          style={{
+            backgroundImage: 'url("backend_background.png")',
+            backgroundSize: 'contain',
+            backgroundPosition: 'right center',
+            backgroundRepeat: 'no-repeat',
+          }}
+          ref={ref}
+          onClick={handleContainerClick}
+        >
+          <div className="flex-1 md:m-5">
+            <h3 className={`text-xl md:text-2xl font-bold mb-3 md:mb-5 ml-1 md:ml-3 ${darkMode ? 'text-white' : 'text-black'}`}>
+              Backend
+            </h3>
+            {backendData.map((skill, index) => (
+              <div key={index} className={`bg-black bg-opacity-20 backdrop-filter backdrop-blur-sm ${darkMode ? 'text-white' : 'text-black'} flex items-center flex-1 ml-1 md:ml-3 md:`}>
+                <img src={skill.icon[0]} alt={`${skill.skill} icon`} className="w-4 h-4 md:w-6 md:h-6" />
+                {/* Opacity Bar */}
+                <div
+                  className={`h-4 w-10/12 bg-gray-300 absolute rounded-lg top-1/2 transform -translate-y-1/2 left-1 md:left-8 right-10 md:right-2 ${darkMode ? 'opacity-30' : 'opacity-10'}`}
+                ></div>
+                {/* Colored Bar */}
+                <div
+                  className={`h-2 md:h-4 ml-1 md:ml-2 rounded-lg mb-1 md:mb-3 mt-1 md:mt-3 ${skill.color} bar ${
+                    inView ? 'animate-grow-bar' : ''
+                  } relative`}
+                  style={{
+                    width: inView ? `${skill.level}%` : '0',
+                    transition: inView ? 'width 1.5s' : '0s',
+                    opacity: '1',
+                  }}
+                >
+                  {inView ? `${skill.level}%` : '0%'}
                 </div>
-              ))}
-            </div>
-            <div className="w-full md:w-2/5 h-48 md:h-full flex items-center justify-center">
-              <img src="backend.png" alt="Backend" className="w-2/3 h-auto" />
-            </div>
+                <span className={`ml-1 ${darkMode ? 'text-white' : 'text-black'}`}>{skill.label}</span>
+              </div>
+            ))}
           </div>
-        </CardFlip>
-      </div>
+          <div className="w-full md:w-2/5 h-24 md:h-40 flex items-center justify-center">
+            <img src="backend.png" alt="Backend" className="w-1/2 h-auto" />
+          </div>
+        </div>
+      </CardFlip>
     </div>
   );
 };
